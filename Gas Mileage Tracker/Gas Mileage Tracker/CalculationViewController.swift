7fc9378e36d9e4035ded032dbbe95caf85f0e18c
jabr9983 <jabr9983@colorado.edu>
2015-02-23 16:11:34 -0700

    xcode files
    Jacob created

---
+++ Gas Mileage Tracker/Gas Mileage Tracker/CalculationViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.swift
+//  Gas Mileage Tracker
+//
+//  Created by Jacob Brauchler on 2/18/15.
+//  Copyright (c) 2015 3308. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func calculategasmileage(milesdriven: Int, gaspurchased: Int) -> Int {
+        var calculation = milesdriven / gaspurchased
+        return calculation
+    
+    }
+
+
+}
+
---
+++ Gas Mileage Tracker/Gas Mileage Tracker/TrackerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gas Mileage Tracker
+//
+//  Created by Jacob Brauchler on 2/18/15.
+//  Copyright (c) 2015 3308. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
