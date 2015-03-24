da0daa40581796c561620344439d2ae4f86bdb38
Ryan Craig <Ryan@ryans-mbp.attlocal.net>
2015-03-23 17:17:41 -0700

    useable data from textfields, button does calculation

--- Gas Mileage Tracker/Gas Mileage Tracker/CalculationViewController.swift
+++ Gas Mileage Tracker/Gas Mileage Tracker/CalculationViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class FirstViewController: UIViewController, UITextFieldDelegate {
 
+    // initialise variables
+    @IBOutlet weak var gallons: UITextField!
+    var gallonsTotal: Int!
+    @IBOutlet weak var miles: UITextField!
+    var milesDriven: Int!
+    var calculation: Int!
+
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,12 +30,33 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.view.endEditing(true)
+        
     }
     
-    func calculategasmileage(milesdriven: Int, gaspurchased: Int) -> Int {
-        var calculation = milesdriven / gaspurchased
+    //when button is presed, close keyboard, get variables, calculate mpg
+    @IBAction func Calculate() {
+        self.view.endEditing(true)
+        getVariables()
+        calculategasmileage()
+        
+        
+    }
+    
+    //get string from UITextField
+    func getVariables(){
+        //get desired string from UITextField convert to int
+        milesDriven = miles.text.toInt()
+        gallonsTotal = gallons.text.toInt()
+        
+    }
+    
+    
+    func calculategasmileage() -> Int {
+        calculation = milesDriven / gallonsTotal
+        println(calculation)
         return calculation
     
     }
