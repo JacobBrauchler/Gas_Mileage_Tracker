b2d50a6c041915ea14d87a01051dcc8ea04ac5d9
jabr9983 <jabr9983@colorado.edu>
2015-04-06 18:47:38 -0600

    Started working on getting the table to store values left notes so anyone can work on it. Jake

--- Gas Mileage Tracker/Gas Mileage Tracker/TrackerViewController.swift
+++ Gas Mileage Tracker/Gas Mileage Tracker/TrackerViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var TableView: UITableView!
+    var milesDriven: Float32!
+    var calculation: Float32!
+    var gallonsTotal: Float32!
+    var sortedKeys:[Int] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,20 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    //http://www.raywenderlich.com/75289/swift-tutorial-part-3-tuples-protocols-delegates-table-views
+    //hey guys I found code similar to what I think we want to do, I got started but don't have it working quite yet. 
+    //func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       // return sortedKeys.count
+    //}
+    
+    func TableView(TableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: nil)
+        
+        
+        cell.textLabel?.text = "\(calculation)%:"
+        cell.detailTextLabel?.text = String(format:"milesDriven: $%0.2f, gallonsTotal: $%0.2f, calculation",milesDriven, gallonsTotal, calculation)
+        return cell
     }
 
 
