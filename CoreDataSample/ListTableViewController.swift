ca247e6259de2234af4b03f0fa24b49564cf1ee4
jabr9983 <jabr9983@colorado.edu>
2015-04-23 19:45:23 -0600

    minor fix

--- CoreDataSample/ListTableViewController.swift
+++ CoreDataSample/ListTableViewController.swift
@@ -77,20 +77,20 @@ class ListTableViewController: UITableViewController , NSFetchedResultsControlle
         
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
         let menu = self.fetchedResultsController.objectAtIndexPath(indexPath) as Menu
-        cell.textLabel.text = menu.menuText
+        cell.textLabel?.text = menu.menuText
         return cell
     }
 
 
-    /*
+   /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return NO if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
@@ -100,8 +100,8 @@ class ListTableViewController: UITableViewController , NSFetchedResultsControlle
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
+    
+*/
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
